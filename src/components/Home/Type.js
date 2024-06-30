601ac32d76310b07407583ce7942362e22e49b83
EinavFerrera <134479183+EinavFerrera@users.noreply.github.com>
2024-06-30 18:41:28 +0300

    first update- changed the Home page text

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,9 +7,9 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Web Designer",
+          "PERN Stack Developer",
+          "Product Manager",
         ],
         autoStart: true,
         loop: true,
