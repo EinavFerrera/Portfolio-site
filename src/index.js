726417ae99ed82ff427c1f64ec8be52caaaee725
EinavFerrera <134479183+EinavFerrera@users.noreply.github.com>
2024-07-03 14:22:14 +0300

    chore: Remove commented code in Navbar.js and update Firebase configuration in index.js

--- src/index.js
+++ src/index.js
@@ -8,14 +8,15 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDkwO76IRHnGsqO29EstBCLF638MPcYNZw",
-  authDomain: "einavferrera-portfolio.firebaseapp.com",
-  projectId: "einavferrera-portfolio",
-  storageBucket: "einavferrera-portfolio.appspot.com",
-  messagingSenderId: "909177387468",
-  appId: "1:909177387468:web:c37c76bb22432e3d2cc569",
-  measurementId: "G-JMD4TDYDQ8",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
